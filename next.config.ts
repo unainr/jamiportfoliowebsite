017fcf149fe6024b595df722d0b2118e0e12a6ef
UNAINR <jamiraza359@gmail.com>
2024-12-26 12:37:45 +0500

    main

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,20 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+
+  async headers() {
+    return [
+      {
+        source: "/studio/:path*", // Match the Sanity Studio route
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, max-age=0, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
